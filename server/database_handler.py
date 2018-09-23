bea36133e27737edfb611c86e7dff84fcccf9051
Constantine <cmen.itis@gmail.com>
2018-09-23 17:40:18 +0300

    correct database name

--- server/database_handler.py
+++ server/database_handler.py
@@ -6,7 +6,7 @@ class DatabaseHandler(object):
     def __init__(self, db_name='person'):
         super(DatabaseHandler, self).__init__()
         self.db_name = db_name
-        self.conn = sqlite3.connect('{}'.format(self.db_name))
+        self.conn = sqlite3.connect('{}.db'.format(self.db_name))
         self.cursor = self.conn.cursor()
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS {} (
