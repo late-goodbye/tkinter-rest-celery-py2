4d2592f50c9951f95b702ab482e473ba2f2e8e32
Constantine <cmen.itis@gmail.com>
2018-09-24 17:58:58 +0300

    add connection processing in the separated thread

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
 import socket
+import thread
 from database_handler import DatabaseHandler
 from tasks import add_person, generate_records
 
@@ -11,26 +12,31 @@ class Server(object):
         self.port = port
         self.dh = DatabaseHandler()
 
+    def process_connection(self, conn, addr):
+        print 'Process {} started'.format(thread.get_ident())
+        print 'Connected by {}'.format(addr)
+        self.data = tuple(conn.recv(1024).split('~'))
+        if self.data[0] == 'add':
+            add_person.delay(self.data[1:])
+        elif self.data[0] == 'gen':
+            print 'Put generate records task to query'
+            records = generate_records.delay(addr)
+        elif self.data[0] == 'get':
+            # self.return_records()
+            print 'Return records'
+        else:
+            print "Wrong command"
+
+        print 'Received: {}'.format(self.data)
+        conn.close()
+
     def run(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind((self.host, self.port))
         sock.listen(5)
         while True:
             conn, addr = sock.accept()
-            print 'Connected by {}'.format(addr)
-            self.data = tuple(conn.recv(1024).split('~'))
-            if self.data[0] == 'add':
-                add_person.delay(self.data[1:])
-            elif self.data[0] == 'gen':
-                print 'Put generate records task to query'
-                records = generate_records.delay(addr)
-            elif self.data[0] == 'get':
-                # self.return_records()
-                print 'Return records'
-            else:
-                print "Wrong command"
-
-            print 'Received: {}'.format(self.data)
+            thread.start_new_thread(self.process_connection, (conn, addr))
         sock.close()
 
 
