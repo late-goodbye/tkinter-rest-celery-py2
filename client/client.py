42aabbe5c9ab8f127d9d3ddc13aeb5fc427c492e
Constantine <cmen.itis@gmail.com>
2018-09-29 20:36:31 +0300

    add get command handling

--- client/client.py
+++ client/client.py
@@ -1,36 +1,23 @@
 # coding=utf8
 import Tkinter
-from Tkinter import END
 import socket
 import sys
-import thread
-from time import sleep
 import datetime
 
-
 class Client(object):
-    """
-    Client is independent class working in infinite loop with gui.
-    During an instance initiation host and port have to be defined.
-    The default values are 'localhost' and 9000
-    Main window contains three buttons
-
-    To prevent a user from wrong actions the buttons may be activated and
-    deactivated.
-
-    The app contains three additional windows:
-    * adding record to specify new record's information
-    * watch records received from a server
-    * watch error has been occured
-    """
+    """docstring for Client."""
     def __init__(self, host='localhost', port=9000):
         super(Client, self).__init__()
         self.host = host
         self.port = port
-        self.records_requested_flag = False
 
+        self.connector = '~'
+        self.records_filename = ''
+
+        self.create_main_window()
+
+    def create_main_window(self):
         self.root = Tkinter.Tk()
-
         self.btn_width = 50
         self.gen_btn = Tkinter.Button(
             self.root,
@@ -46,7 +33,6 @@ class Client(object):
             self.root,
             text=u'Получить список записей',
             width=self.btn_width)
-        self.get_btn['state'] = 'disabled'
 
         self.add_btn.bind('<Button-1>', self.open_form)
         self.gen_btn.bind('<Button-1>', self.request_records)
@@ -57,14 +43,7 @@ class Client(object):
         self.get_btn.pack()
 
     def open_form(self, event):
-        """
-        Window for adding new record
-        After form filled and Save button pushed a thread with new connection
-        will be created to send data on the server. Server will close this
-        connection then the data received
-        The cancel button just destroys the window without any changes
-        """
-        self.form = Tkinter.Tk()
+        self.form = Tkinter.Toplevel()
         Tkinter.Label(self.form, text=u'Фамилия').grid(row=0, column=0)
         Tkinter.Label(self.form, text=u'Имя').grid(row=1, column=0)
         Tkinter.Label(self.form, text=u'Отчество').grid(row=2, column=0)
@@ -83,176 +62,95 @@ class Client(object):
         self.birth_date.grid(row=3, column=1)
 
         self.save_btn = Tkinter.Button(
-            self.form, text=u'Сохранить', command=self.send_data)
+            self.form, text=u'Сохранить', command=self.send_person_data)
         self.save_btn.grid(row=5, column=0)
 
         self.cancel_btn = Tkinter.Button(
             self.form, text=u'Отмена', command=self.form.destroy)
         self.cancel_btn.grid(row=5, column=1)
 
-    def run(self):
-        """ The entry point """
-        self.root.mainloop()
-
-    def process_records(self):
-        """
-        The main work infinte loop for generation and receiving records
-        These functions performed by transfering short messages to and from
-        the server
-        To control current state the variable records_requested_flag is used
-        Then it's value becomes True get request will be sent to server
-        """
+    def connect_to_server(self):
         try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         except socket.error, e:
             self.log('Error creating socket', e)
 
         try:
-            sock.connect((self.host, self.port))
+            self.sock.connect((self.host, self.port))
         except socket.gaierror, e:
             self.log('Address-related error connecting to server', e)
         except socket.error, e:
             self.log('Connection error', e)
 
+    def send_person_data(self):
+        self.connect_to_server()
+        message = self.connector.join([field.get() for field in
+            [self.lastname, self.firstname, self.middlename, self.birth_date]])
+        self.form.destroy()
+        print 'message: {}'.format(message)
         try:
-            sock.sendall('gen')
+            self.sock.sendall('add{}{}\n'.format(self.connector, message))
         except socket.error, e:
             self.log('Error sending data', e)
+        print 'message has been sent'
 
-        state = ''
-        while True:
-            try:
-                data = tuple(sock.recv(1024).split('~'))
-            except socket.error, e:
-                self.log('Error receiving data', e)
-            print data
-            if not data[0]:
-                break
-            elif data[0] == 'state':
-                state = data[1]
-                if state == 'SUCCESS':
-                    self.get_btn['state'] = 'active'
-                    self.gen_btn['state'] = 'active'
-                    self.gen_btn['text'] = (
-                        u'Сгенерировать список записей (Готово)')
-                    try:
-                        sock.sendall('ok')
-                    except socket.error, e:
-                        self.log('Error sending data', e)
-                elif state != 'SUCCESS':
-                    self.gen_btn['text'] = (
-                        u'Сгенерировать список записей (Запрос обрабатывается)')
-                    print 'sending state request'
-                    try:
-                        sock.sendall('state?')
-                    except socket.error, e:
-                        self.log('Error sending data', e)
-            elif state != 'SUCCESS':
-                print 'sending state request'
-                try:
-                    sock.sendall('state?')
-                except socket.error, e:
-                    self.log('Error sending data', e)
-            elif self.records_requested_flag:
-                print 'sending get signal'
-                try:
-                    sock.sendall('get')
-                except socket.error, e:
-                    self.log('Error sending data', e)
-                self.records_requested_flag = False
-            elif data[0] == 'rec':
-                try:
-                    sock.send('go')
-                except socket.error, e:
-                    self.log('Error sending data', e)
-                try:
-                    data = sock.recv(1024)
-                except socket.error, e:
-                    self.log('Error receiving data', e)
-                records = ''
-                while data:
-                    records += data
-                    try:
-                        data = sock.recv(1024)
-                    except socket.error, e:
-                        self.log('Error receiving data', e)
-                else:
-                    records = records.strip().split('\n')
-                    print records
-                    print 'Bye, {}:{}'.format(self.host, self.port)
-                    break
-            else:
-                try:
-                    sock.sendall('ok')
-                except socket.error, e:
-                    self.log('Error sending data', e)
-        sock.close()
-        if records:
-            self.show_records(records)
-
-    def show_records(self, records):
-        """ Shows records from list on new window """
-        records_window = Tkinter.Toplevel()
-        text_box = Tkinter.Text(records_window, wrap='word')
-        scrollbar = Tkinter.Scrollbar(records_window)
-
-        scrollbar['command'] = text_box.yview
-        text_box['yscrollcommand'] = scrollbar.set
-
-        text_box.pack(side='left')
-        scrollbar.pack(side='right')
-        for record in records:
-            text_box.insert('end', '{}\n'.format(record))
-        text_box['state'] = 'disabled'
-        self.gen_btn['text'] = u'Сгенерировать список записей'
+        try:
+            res = self.sock.recv(1)
+            print res
+        except socket.error, e:
+            res = False
+            self.log('Error receiving data', e)
+        finally:
+            self.sock.close()
+            self.show_add_person_result(success=res)
 
     def request_records(self, event):
-        """ Starts new thread for records transfering and controls buttons """
-        if self.gen_btn['state'] == 'active':
-            thread.start_new_thread(self.process_records, tuple())
-            self.gen_btn['state'] = 'disabled'
-
-    def receive_records(self, event):
-        """ Changes the flag to sent request for records list """
-        if self.get_btn['state'] == 'active':
-            self.records_requested_flag = True
-            self.get_btn['state'] = 'disabled'
-
-    def send_data(self):
-        """ Creates short connection to send new record on server """
+        self.connect_to_server()
         try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        except socket.error, e:
-            self.log('Error creating socket', e)
-
-        try:
-            sock.connect((self.host, self.port))
-        except socket.gaierror, e:
-            self.log('Address-related error connecting to server', e)
-        except socket.error, e:
-            self.log('Connection error', e)
-
-        connector = '~'
-        message = connector.join([field.get() for field in
-            [self.lastname, self.firstname, self.middlename, self.birth_date]])
-
-        try:
-            sock.sendall('add{}{}'.format(connector, message))
+            self.sock.sendall('gen\n')
         except socket.error, e:
             self.log('Error sending data', e)
-        
-        sock.close()
-        self.form.destroy()
+
+        try:
+            self.records_filename = self.sock.recv(1024)
+        except socket.error, e:
+            self.log('Error receiving data', e)
+
+        print 'Records filename: {}'.format(self.records_filename)
+
+
+    def receive_records(self, event):
+        pass
+
+    def show_add_person_result(event, success):
+        print success
+        result_window = Tkinter.Toplevel()
+        label_text = u'Успешно' if success else u'Ошибка'
+        print label_text
+        label = Tkinter.Label(result_window, text=label_text)
+        ok_btn = Tkinter.Button(
+            result_window, text=u'OK', command=result_window.destroy)
+        label.pack()
+        ok_btn.pack()
+
 
     def log(self, desc, e):
-        """ If an exception catched adds information into log file and resets gui"""
+        """ Adds information into log file in case of exception catched """
         time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         message = '{} -- {}, {}'.format(time, desc, e)
         with open('log.txt', 'a') as log:
             log.write('{}\n'.format(message))
-        self.reset_gui()
         self.show_error(message)
         sys.exit(1)
+
+    def load_logs(self, event):
+        """ Clears text box in log window and fills it with all logs """
+        self.text_box['state'] = 'normal'
+        self.text_box.delete('1.0', Tkinter.END)
+        with open('log.txt', 'r') as log:
+            for record in log:
+                self.text_box.insert('end', record)
+        self.text_box['state'] = 'disabled'
 
     def show_error(self, message):
         """
@@ -274,20 +172,10 @@ class Client(object):
         self.log_btn.bind('<Button-1>', self.load_logs)
         self.log_btn.pack(side='bottom')
 
-    def load_logs(self, event):
-        """ Clears text box in log window and fills it with all logs """
-        self.text_box['state'] = 'normal'
-        self.text_box.delete('1.0', END)
-        with open('log.txt', 'r') as log:
-            for record in log:
-                self.text_box.insert('end', record)
-        self.text_box['state'] = 'disabled'
+    def run(self):
+        """ The entry point """
+        self.root.mainloop()
 
-    def reset_gui(self):
-        """ Set main menu buttons into default state """
-        self.gen_btn['text'] = u'Сгенерировать список записей'
-        self.get_btn['state'] = 'disabled'
-        self.gen_btn['state'] = 'active'
 
 if __name__ == '__main__':
     client = Client()
--- server/server.py
+++ server/server.py
@@ -1,123 +1,76 @@
-import socket
-import thread
-from time import sleep
+import SocketServer
 from database_handler import DatabaseHandler
 from tasks import add_person, generate_records
 import os
 import sys
+from time import ctime
+from shutil import rmtree
+import hashlib
 
 
-class Server(object):
-    """
-    Server is independent class working in infinite cycle.
-    It uses the DatabaseHandler to operate sqlite3 database.
-    Host and port parameters may be defined during initializing the instance.
-    Default values for host and port are 'localhost' and 9000 respectively.
-    """
-    def __init__(self, host='localhost', port=9000):
-        super(Server, self).__init__()
-        self.host = host
-        self.port = port
-        self.dh = DatabaseHandler()
-
-    def process_connection(self, conn, addr):
-        """
-        The main working loop.
-        It works synchronized with an Client instance by transfering short
-        messages.
-        The both instances send messages to each other every 1 second.
-        If the message consists of several words the words divided by a '~' char.
-        The first word of the message is a command.
-        If message contains new record then it starts with 'add' command
-        This command executes in short way without infinite loop
-
-        'gen' command starts records file generation task with celery.
-        If the task started the client sends request for task status
-        The server responses to these requests. If the task status becomes
-        'SUCCESS' the client stops requesting task status.
-
-        'get' command starts sending the file cycle
-        During this phase, the servet notifies the client about sending
-        After server has got response from the client it sends all records
-        from the records file to the client. After this connection will close.
-
-        After closing the connection the records file will be removed
-        """
-        print 'Process {} started'.format(thread.get_ident())
-        print 'Connected by {}'.format(addr)
-        self.data = tuple(conn.recv(1024).split('~'))
-        state = ''
+class CustomTCPHandler(SocketServer.StreamRequestHandler):
+    def handle(self):
+        self.data = tuple(self.rfile.readline().strip().split('~'))
         if self.data[0] == 'add':
-            add_person.delay(self.data[1:])
-            print 'Received: {}'.format(self.data)
+            self.wfile.write(1 if self.handle_add() else 0)
+        elif self.data[0] == 'gen':
+            filename = self.handle_gen()
+            self.wfile.write(filename)
+        elif self.data[0] == 'get':
+            try:
+                self.handle_get(self.data[1])
+            except IndexError:
+                print 'No filename in get request'
         else:
-            while True:
-                print self.data
-                if not self.data[0]:
-                    break
-                elif self.data[0] == 'state?':
-                    if task:
-                        state = task.state
-                    else:
-                        state = 'Not started'
-                    conn.sendall('~'.join(['state', state]))
-                elif self.data[0] == 'gen':
-                    print 'Put generate records task to query'
-                    conn.sendall('ok')
-                    task = generate_records.delay(addr)
-                    print task.ready()
-                    print task.state
-                elif self.data[0] == 'get':
-                    if os.path.isfile(os.path.join(
-                        os.getcwd(),
-                        'records-{}-{}.txt'.format(addr[0], addr[1]))):
-                        conn.send('rec')
-                        response = conn.recv(10)
-                        if response == 'go':
-                            with open(
-                                'records-{}-{}.txt'.format(addr[0], addr[1]), 'r'
-                            ) as records_file:
-                                for line in records_file:
-                                    conn.send(line)
-                                break
-                    else:
-                        print 'The records are not generated yet'
-                        print 'records-{}-{}.txt'.format(addr[0], addr[1])
-                        conn.sendall('ok')
-                else:
-                    conn.sendall('ok')
-                sleep(1)
-                self.data = tuple(conn.recv(1024).split('~'))
-        os.remove('records-{}-{}.txt'.format(addr[0], addr[1]))
-        print 'Bye, {}:{}'.format(addr[0], addr[1])
-        conn.close()
+            self.handle_unknown()
 
-    def clear_directory(self):
-        """
-        Remove outdated records text files if they exist
-        These files produced by previous runs and not deleted during error
-        occured
-        """
-        files = os.listdir(os.getcwd())
-        for file in files:
-            if 'records-' in file and '.txt' in file:
-                os.remove(os.path.join(os.getcwd(), file))
+    def handle_add(self):
+        try:
+            return add_person.delay(self.data[1:])
+        except Exception as e:
+            raise 'Error add record to database: {}'.format(e)
+        else:
+            return False
 
-    def run(self):
-        """
-        The entry point to start work
-        The ifinite loop producing new thread in case of connection accepted
-        """
-        self.clear_directory()
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind((self.host, self.port))
-        sock.listen(5)
-        while True:
-            conn, addr = sock.accept()
-            thread.start_new_thread(self.process_connection, (conn, addr))
-        sock.close()
+    def handle_gen(self):
+        h = hashlib.md5()
+        h.update(ctime())
+        h.update(''.join(str(self.request.getpeername())))
+        filename = h.hexdigest()
+        print filename
+        try:
+            filepath = os.path.join(
+                os.path.join(os.getcwd(), 'records'), filename)
+            generate_records(filepath)
+            return filename
+        except Exception as e:
+            raise e
+        else:
+            return '0'
+
+    def handle_get(self, filename):
+        pass
+
+    def handle_unknown(self):
+        raise RuntimeError('Can not recognize received command')
+
+def clear_directory():
+    """
+    Remove outdated records text files if they exist
+    These files produced by previous runs and not deleted during error
+    occured
+    """
+    records_directory = os.path.join(os.getcwd(), 'records')
+    if os.path.isdir(records_directory):
+        rmtree(records_directory)
+        print 'Records cleared'
+    os.mkdir(records_directory)
 
 
 if __name__ == '__main__':
-    server = Server()
-    server.run()
+    clear_directory()
+
+    host, port = 'localhost', 9000
+    server = SocketServer.TCPServer((host, port), CustomTCPHandler)
+    server.serve_forever()
+    server.allow_reuse_address = True
